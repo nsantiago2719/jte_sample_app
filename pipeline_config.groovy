99a476e37ba2f453bec7470df3e60be657729053
nsantiago2719 <nsantiago2719@gmail.com>
2019-11-27 12:05:37 +0800

    update pipeline config

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,10 +1,3 @@
-/*
-  specify which libraries to load: 
-    In the Governance Tier configuration file, 
-    these should be configurations common across 
-    all apps governed by this config. 
-*/
 libraries{
-  merge = true
   ruby
 }
